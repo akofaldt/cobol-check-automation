b6b57da2cd10c5c823dfc2d1592e5b031a87408d
André Kofaldt <91030659+akofaldt@users.noreply.github.com>
2025-11-25 22:40:03 +0100

    Added connection options

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -4,19 +4,21 @@
 
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+ZOWE_OPTS=--user "$ZOWE_USERNAME" --password "$ZOWE_PASSWORD" --host "$ZOWE_HOST" --port "$ZOWE_PORT" --reject-unauthorized false
 
 # Check if directory exists , create if it doesn't
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME"
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+echo zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME" $ZOWE_OPTS
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME" $ZOWE_OPTS
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_OPTS &>/dev/null; then
   echo "Directory does not exist. Creating it ..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck $ZOWE_OPTS
 else
   echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.19.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.19.jar" $ZOWE_OPTS
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_OPTS
