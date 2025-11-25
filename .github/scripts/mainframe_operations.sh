2f8ae06b2793d68f52af4d9b3c833ac6f010a5f4
André Kofaldt <andre@kofaldt.de>
2025-11-25 23:09:43 +0100

    Added script

---
+++ .github/scripts/mainframe_operations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# mainframe_operations.sh
+
+# Set up environment
+#export PATH=$PATH:/usr/lpp/java/J8.0_64/bin
+#export JAVA_HOME=/usr/lpp/java/J8.0_64
+export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
+
+java -version
+
+zowe --version
